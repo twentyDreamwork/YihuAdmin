805f6ee61107689c3463e418900c571f450ff3b8
leqi <693472577@qq.com>
2019-09-08 19:29:32 +0800

    ..update

--- admin-yihu/src/main/java/cn/yihu/microboot/mapper/goods/CarouselMapper.java
+++ admin-yihu/src/main/java/cn/yihu/microboot/mapper/goods/CarouselMapper.java
@@ -3,6 +3,7 @@ package cn.yihu.microboot.mapper.goods;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -23,7 +24,30 @@ public interface CarouselMapper extends BaseMapper<Carousel>{
 	@Select("select * from xe_carousel ")
 	List<Carousel> queryAllCarousel();
 	
+	/**
+	 * 根据id删除
+	 * @param id
+	 * @return
+	 */
 	@Delete("delete from xe_carousel where id=#{id}")
 	int deletebyid(@Param("id")int id);
+	
+	/**
+	 * 统计数量
+	 * @return
+	 */
+	@Select("select count(*) from xe_carousel")
+	int count();
 
+	/**
+	 * 页面查询
+	 * @param pageno
+	 * @param pagesize
+	 * @return
+	 */
+	@Select("select * from xe_carousel limit #{pageno},#{pagesize}")
+	List<Carousel> listpage(@Param("pageno")int pageno,@Param("pagesize")int pagesize);
+	
+
+	int insertSelective(Carousel carousel);
 }
